fb93d09f3ba1d3348e456f38ab2c67f2caa0b347
danielpinto01 <daniel.pinto01@uptc.edu.co>
2018-04-22 09:42:05 -0500

    Creacion de Botones de Stop y Reports

--- src/views/MainWindow.java
+++ src/views/MainWindow.java
@@ -2,10 +2,13 @@ package views;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.GridLayout;
 import java.util.ArrayList;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 import controllers.Controller;
 import models.Person;
@@ -14,6 +17,9 @@ public class MainWindow extends JFrame{
 
 	private static final long serialVersionUID = 1L;
 	private JPanelInit jPanelInit;
+	private JPanel jPanelButtons;
+	private JButton btnStop;
+	private JButton btnReports;
 
 	public MainWindow(Controller controller) {
 		setBackground(Color.BLACK);
@@ -40,6 +46,13 @@ public class MainWindow extends JFrame{
 	public void init() {
 		jPanelInit = new JPanelInit();
 		add(jPanelInit, BorderLayout.CENTER);
+		jPanelButtons = new JPanel();
+		jPanelButtons.setLayout(new GridLayout(2, 1));
+		btnStop = new JButton("Stop");
+		btnReports = new JButton("Reports");
+		jPanelButtons.add(btnStop);
+		jPanelButtons.add(btnReports);
+		add(jPanelButtons, BorderLayout.EAST);
 	}
 	
 //	public void addPerson(Person person) {
