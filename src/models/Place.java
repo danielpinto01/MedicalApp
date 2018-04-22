3c447057dd2058b16fbe76de7a24b04348c8f734
danielpinto01 <daniel.pinto01@uptc.edu.co>
2018-04-21 23:34:17 -0500

    Se agrego la clase Place con informacion de los servicios

--- src/models/Place.java
+++ src/models/Place.java
@@ -4,9 +4,12 @@ import javax.swing.ImageIcon;
 
 public enum Place {
 	
-	QUERY(1000, 50, "Consulta", "/images/consulta.png"), QUOTE(50, 800, "Cita","/images/quote.png"), 
-	EMERGENCY_QUERY(300, 50, "Emergencia", "/images/emergency.png"), NORMAL_QUERY(500, 50, "General", "/images/general.png"), 
-	PHARMACY(400, 400, "Farmacia", "/images/farmacia.png"), BILLING(900, 600, "facturacion", "/images/facturacion.png"), 
+	QUERY(1000, 50, "Consulta", "/images/consulta.png"), 
+	QUOTE(50, 800, "Cita","/images/quote.png"), 
+	EMERGENCY_QUERY(300, 50, "Emergencia", "/images/emergency.png"), 
+	NORMAL_QUERY(500, 50, "General", "/images/general.png"), 
+	PHARMACY(400, 400, "Farmacia", "/images/farmacia.png"), 
+	BILLING(900, 600, "facturacion", "/images/facturacion.png"), 
 	INIT(1400, 400, "facturacion", "/images/init.png");
 	
 	private int posX, posY;
