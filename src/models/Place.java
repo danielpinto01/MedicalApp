45fd027044b60ecafb144ee62d7c9e02c9d5833c
danielpinto01 <daniel.pinto01@uptc.edu.co>
2018-04-23 09:50:32 -0500

    Modificacion de iconos

--- src/models/Place.java
+++ src/models/Place.java
@@ -4,14 +4,14 @@ import javax.swing.ImageIcon;
 
 public enum Place {
 	
-	QUERY(1400, 50, "Consulta", "/images/consulta.png"), 
-	QUOTE(1400, 600, "Cita","/images/quote.png"), 
-	EMERGENCY_QUERY(1000, 300, "Emergencia", "/images/emergency.png"), 
-	NORMAL_QUERY(500, 100, "General", "/images/general.png"), 
-	PHARMACY(800, 800, "Farmacia", "/images/farmacia.png"), 
-	BILLING(300, 500, "facturacion", "/images/facturacion.png"), 
-	INIT(1800, 400, "Entrada", "/images/init.png"),
-	EXIT(-100, 400, "Salida", "/images/exit.png");
+	QUERY(1400, 50, "Consulta", "/images/consulta1.png"), 
+	QUOTE(1400, 600, "Cita","/images/citas1.png"), 
+	EMERGENCY_QUERY(1000, 300, "Emergencia", "/images/emergencia1.png"), 
+	NORMAL_QUERY(500, 100, "General", "/images/general1.png"), 
+	PHARMACY(800, 800, "Farmacia", "/images/farmacia1.png"), 
+	BILLING(300, 500, "facturacion", "/images/facturacion1.png"), 
+	INIT(1900, 400, "Entrada", "/images/init.png"),
+	EXIT(-200, 400, "Salida", "/images/exit.png");
 	
 //	QUERY(1000, 50, "Consulta", "/images/consulta.png"), 
 //	QUOTE(50, 800, "Cita","/images/quote.png"), 
--- src/views/JPanelInit.java
+++ src/views/JPanelInit.java
@@ -20,7 +20,8 @@ public class JPanelInit extends JPanel{
 	//	private Person person;
 
 	public JPanelInit() {
-		setBackground(Color.PINK);
+//		setBackground(Color.decode("#64B5F6"));
+		setBackground(Color.DARK_GRAY);
 		personList = new ArrayList<>();
 		productList = new ArrayList<>();
 	}
@@ -37,16 +38,28 @@ public class JPanelInit extends JPanel{
 		g.drawImage(Place.BILLING.getImage().getImage(), Place.BILLING.getPosX(), Place.BILLING.getPosY(), 150, 150, this);
 		g.drawImage(Place.INIT.getImage().getImage(), Place.INIT.getPosX(), Place.INIT.getPosY(), 150, 150, this);
 		g.drawImage(Place.EXIT.getImage().getImage(), Place.EXIT.getPosX(), Place.EXIT.getPosY(), 150, 150, this);
+		
+		g.drawImage(new ImageIcon(getClass().getResource("/images/welcome.png")).getImage(), 1750,  80, 150, 150, this);
+		g.drawImage(new ImageIcon(getClass().getResource("/images/welcome.png")).getImage(), 1750,  210, 150, 150, this);
+		g.drawImage(new ImageIcon(getClass().getResource("/images/welcome.png")).getImage(), 1750,  350, 150, 150, this);
+		g.drawImage(new ImageIcon(getClass().getResource("/images/welcome.png")).getImage(), 1750,  490, 150, 150, this);
+		g.drawImage(new ImageIcon(getClass().getResource("/images/welcome.png")).getImage(), 1750,  630, 150, 150, this);
+		g.drawImage(new ImageIcon(getClass().getResource("/images/welcome.png")).getImage(), 1750,  770, 150, 150, this);
 
+		g.drawImage(new ImageIcon(getClass().getResource("/images/exit.png")).getImage(), 0,  400, 130, 130, this);
+		
 		for (Person person : personList) {
 			//			g.drawLine(1900, 400, person.getPosX(), person.getPosY());
 			if (person.getPlaceInit() == Place.PHARMACY) {
-				g.drawImage(new ImageIcon(getClass().getResource("/images/personLeft.gif")).getImage(), person.getPosX(),  person.getPosY(), 60, 60, this);
-				g.drawImage(new ImageIcon(getClass().getResource("/images/pill.png")).getImage(), person.getPosX(),  person.getPosY(), 20, 20, this);
+				g.drawImage(new ImageIcon(getClass().getResource("/images/personLeft.gif")).getImage(), person.getPosX(),  person.getPosY(), 90, 90, this);
+				g.drawImage(new ImageIcon(getClass().getResource("/images/pill.png")).getImage(), person.getPosX(),  person.getPosY(), 40, 40, this);
+			}else if (person.getPlaceInit() == Place.NORMAL_QUERY) {
+				g.drawImage(new ImageIcon(getClass().getResource("/images/personRight.gif")).getImage(), person.getPosX(),  person.getPosY(), 90, 90, this);
+				
 			}else {
 				g.drawImage(new ImageIcon(getClass().getResource("/images/personLeft.gif")).getImage(), person.getPosX(),  person.getPosY(), 90, 90, this);
 			}
-			//			g.drawString(String.valueOf(person.getId()), person.getPosX(), person.getPosY());
+			//		g.drawString(String.valueOf(person.getId()), person.getPosX(), person.getPosY());
 		}
 		for (Product product : productList) {
 			g.drawImage(new ImageIcon(getClass().getResource("/images/pill.png")).getImage(), product.getPosX(),  product.getPosY(), 50, 50, this);
